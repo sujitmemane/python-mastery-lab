dcab40ac1000ee0ee7046267b72da5f1e646bbad
sujitmemane <officialsujitmemane@gmail.com>
2026-08-28 01:01:23 +0530

    feat: completed range chapter

--- 02-control-flow/04-range/practice.py
+++ 02-control-flow/04-range/practice.py
@@ -1,12 +1,17 @@
 """Practice: use range() to repeat a known number of times."""
 
 # Level 1 — Print numbers from 0 through 9.
-
-
-# Level 2 — Print numbers from 1 through 10.
+for i in range(0,10):
+    print(i)
 
 
 # Level 3 — Print all even numbers from 2 through 20.
-
+for i in range(0,21):
+    if i%2==0:
+        print(i)
 
 # Level 4 — Calculate 5! (5 * 4 * 3 * 2 * 1) using a loop and range().
+sum=1
+for i in range(1,6):
+    sum*=i
+print(sum)
