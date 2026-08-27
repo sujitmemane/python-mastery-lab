8b2170d18499dd69cb868feb5f3d57992faca566
sujitmemane <officialsujitmemane@gmail.com>
2026-08-28 01:48:02 +0530

    feat: implement nested loops examples with print statements

--- 02-control-flow/07-nested-loops/practice.py
+++ 02-control-flow/07-nested-loops/practice.py
@@ -3,7 +3,8 @@
 # Level 1 - Print a 3 by 4 rectangle of stars.
 for row in range(3):
     for column in range(4):
-        pass
+        print("*")
+    print("\n")
 
 
 # Level 2 - Print this right triangle:
@@ -11,13 +12,15 @@ for row in range(3):
 # * *
 # * * *
 for row in range(1, 4):
-    pass
+    for col in range(1,row):
+        print("*")
+    print("\n")
 
 
 # Level 3 - Print a multiplication table from 1 x 1 through 3 x 3.
 for first in range(1, 4):
     for second in range(1, 4):
-        pass
+        print(f"{first} X {second} ==== {first*second}")
 
 
 # Level 4 - Count how many pairs have the same value.
@@ -25,5 +28,6 @@ values = [1, 2, 1, 3]
 matching_pairs = 0
 for first_index in range(len(values)):
     for second_index in range(first_index + 1, len(values)):
-        pass
+        if values[first_index] == values[second_index]:
+            matching_pairs+=1
 print(matching_pairs)
